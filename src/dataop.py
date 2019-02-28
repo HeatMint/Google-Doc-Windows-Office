8a2538eba2d31bd6986641f29a6597a63c53149c
Mike Lu <heatmint2333@gmail.com>
2019-02-28 19:45:26 +0800

    Data Operater Tool

---
+++ src/dataop.py
@@ -0,0 +1,25 @@
+import json
+
+data_file = open('data.json',"r")
+print data_file
+data = json.loads(data_file.read())
+data_file.close()
+print(data)
+
+
+def update():
+    jsondata = json.dumps(data)
+    data_file = open('data.json',"w")
+    data_file.write(jsondata)
+    data_file.close()
+
+
+def add(key,value):
+    data[key] = value
+    print data
+    update()
+
+
+def delete(key):
+    del data[key]
+    update()
